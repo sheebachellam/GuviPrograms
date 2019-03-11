2056c553906a084c7595b2b70e68eed4271d6e8f
sheebachellam <47551379+sheebachellam@users.noreply.github.com>
2019-03-11 09:54:27 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,10 @@
+def leap year(y):
+    if y % 400 == 0:
+        return True
+    if y % 100 == 0:
+        return False
+    if y % 4 == 0:
+        return True
+    else:
+        return False
+print(leap year)
